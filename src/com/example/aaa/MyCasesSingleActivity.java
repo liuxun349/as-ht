5578a023129f5930021e12aa187a25fe298a59a9
LLH <lzliuxun@gmail.com>
2013-08-14 21:43:05 +0800

    first commit

---
+++ src/com/example/aaa/MyCasesSingleActivity.java
@@ -0,0 +1,35 @@
+package com.example.aaa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+public class MyCasesSingleActivity extends Activity implements OnClickListener {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public void onClick(View v) {
+	}
+
+	
+}
