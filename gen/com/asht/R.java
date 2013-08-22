d77962f2225306e334ad01b4bec48afa89c4f788
LLH <lzliuxun@gmail.com>
2013-08-22 18:07:00 +0800

    go on register

--- gen/com/asht/R.java
+++ gen/com/asht/R.java
@@ -11,16 +11,19 @@ public final class R {
     public static final class anim {
         public static final int loading=0x7f040000;
     }
+    public static final class array {
+        public static final int securityQ=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f050005;
-        public static final int light_gray=0x7f050004;
-        public static final int probject_bg=0x7f050001;
-        public static final int title_bg=0x7f050000;
-        public static final int title_text_color=0x7f050002;
-        public static final int transparent=0x7f050006;
-        public static final int white=0x7f050003;
+        public static final int black=0x7f060005;
+        public static final int light_gray=0x7f060004;
+        public static final int probject_bg=0x7f060001;
+        public static final int title_bg=0x7f060000;
+        public static final int title_text_color=0x7f060002;
+        public static final int transparent=0x7f060006;
+        public static final int white=0x7f060003;
     }
     public static final class dimen {
         /** 
@@ -28,9 +31,9 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f060002;
-        public static final int title_height=0x7f060000;
-        public static final int title_text_size=0x7f060001;
+        public static final int activity_horizontal_margin=0x7f070002;
+        public static final int title_height=0x7f070000;
+        public static final int title_text_size=0x7f070001;
     }
     public static final class drawable {
         public static final int about_page_mor=0x7f020000;
@@ -100,64 +103,73 @@ public final class R {
         public static final int yishenhe=0x7f020040;
     }
     public static final class id {
-        public static final int MorePageTableLayout_Client=0x7f090035;
-        public static final int MorePageTableLayout_Favorite=0x7f090034;
-        public static final int btn_exit_cancel=0x7f090001;
-        public static final int btn_exit_ok=0x7f090002;
-        public static final int btn_login=0x7f090028;
-        public static final int btn_myCases_delete=0x7f090007;
-        public static final int btn_myCases_delete_cancel=0x7f09000a;
-        public static final int btn_myCases_delete_ok=0x7f09000b;
-        public static final int care_id=0x7f090019;
-        public static final int cb_isShenHe=0x7f09000e;
-        public static final int cb_isTongGuo=0x7f09000d;
-        public static final int cb_myCases_isTongGuo=0x7f090014;
-        public static final int check_number=0x7f09001c;
-        public static final int console_line_bottom=0x7f090029;
-        public static final int display_passwd=0x7f090026;
-        public static final int empty_cart_view=0x7f090016;
-        public static final int fl_myCases_delete=0x7f090006;
-        public static final int fl_myCases_selected_title=0x7f090003;
-        public static final int forget_passwd=0x7f090027;
-        public static final int get_check_number=0x7f09001f;
-        public static final int gv_myCases=0x7f090005;
-        public static final int iv_myCases_delete=0x7f090015;
-        public static final int iv_myCases_pic1=0x7f090010;
-        public static final int iv_myCases_pic2=0x7f090011;
-        public static final int iv_myCases_pic3=0x7f090012;
-        public static final int iv_myCases_pic4=0x7f090013;
-        public static final int iv_pic2=0x7f09000c;
-        public static final int linear1=0x7f09001d;
-        public static final int ll_myCases_delete_isOk=0x7f090008;
-        public static final int login_input_name=0x7f09001e;
-        public static final int login_input_password=0x7f090024;
-        public static final int login_page_input_name=0x7f090022;
-        public static final int login_page_input_password=0x7f090023;
-        public static final int main_tab_buy=0x7f09002e;
-        public static final int main_tab_car=0x7f09002d;
-        public static final int main_tab_catagory=0x7f09002c;
-        public static final int main_tab_group=0x7f09002a;
-        public static final int main_tab_home=0x7f09002b;
-        public static final int main_tab_more=0x7f09002f;
-        public static final int message_time=0x7f090031;
-        public static final int message_title=0x7f090030;
-        public static final int more_page_row1=0x7f090037;
-        public static final int more_page_row2=0x7f090038;
-        public static final int more_page_row4=0x7f090036;
-        public static final int more_page_row5=0x7f090039;
-        public static final int regist_scroller=0x7f090021;
-        public static final int regist_scroller_relative=0x7f090017;
-        public static final int register_check_number=0x7f090032;
-        public static final int register_next=0x7f090020;
-        public static final int register_phone=0x7f09001b;
-        public static final int register_txv_phone=0x7f09001a;
-        public static final int remember_user=0x7f090025;
-        public static final int top_relative=0x7f090033;
-        public static final int tv_myCases_title=0x7f09000f;
-        public static final int tv_title_back=0x7f090000;
-        public static final int txt_myCases_delte_txt=0x7f090009;
-        public static final int txt_myCases_selected_txt=0x7f090004;
-        public static final int txv_care_id=0x7f090018;
+        public static final int MorePageTableLayout_Client=0x7f0a003e;
+        public static final int MorePageTableLayout_Favorite=0x7f0a003d;
+        public static final int btn_exit_cancel=0x7f0a0001;
+        public static final int btn_exit_ok=0x7f0a0002;
+        public static final int btn_login=0x7f0a0028;
+        public static final int btn_myCases_delete=0x7f0a0007;
+        public static final int btn_myCases_delete_cancel=0x7f0a000a;
+        public static final int btn_myCases_delete_ok=0x7f0a000b;
+        public static final int care_id=0x7f0a0019;
+        public static final int cb_isShenHe=0x7f0a000e;
+        public static final int cb_isTongGuo=0x7f0a000d;
+        public static final int cb_myCases_isTongGuo=0x7f0a0014;
+        public static final int check_number=0x7f0a001c;
+        public static final int console_line_bottom=0x7f0a0029;
+        public static final int display_passwd=0x7f0a0026;
+        public static final int empty_cart_view=0x7f0a0016;
+        public static final int fl_myCases_delete=0x7f0a0006;
+        public static final int fl_myCases_selected_title=0x7f0a0003;
+        public static final int forget_passwd=0x7f0a0027;
+        public static final int get_check_number=0x7f0a001f;
+        public static final int gv_myCases=0x7f0a0005;
+        public static final int iv_myCases_delete=0x7f0a0015;
+        public static final int iv_myCases_pic1=0x7f0a0010;
+        public static final int iv_myCases_pic2=0x7f0a0011;
+        public static final int iv_myCases_pic3=0x7f0a0012;
+        public static final int iv_myCases_pic4=0x7f0a0013;
+        public static final int iv_pic2=0x7f0a000c;
+        public static final int linear1=0x7f0a001d;
+        public static final int ll_myCases_delete_isOk=0x7f0a0008;
+        public static final int login_input_name=0x7f0a001e;
+        public static final int login_input_password=0x7f0a0024;
+        public static final int login_page_input_name=0x7f0a0022;
+        public static final int login_page_input_password=0x7f0a0023;
+        public static final int login_pwd=0x7f0a0032;
+        public static final int login_pwd_agin=0x7f0a0033;
+        public static final int main_tab_buy=0x7f0a002e;
+        public static final int main_tab_car=0x7f0a002d;
+        public static final int main_tab_catagory=0x7f0a002c;
+        public static final int main_tab_group=0x7f0a002a;
+        public static final int main_tab_home=0x7f0a002b;
+        public static final int main_tab_more=0x7f0a002f;
+        public static final int message_time=0x7f0a0031;
+        public static final int message_title=0x7f0a0030;
+        public static final int more_page_row1=0x7f0a0040;
+        public static final int more_page_row2=0x7f0a0041;
+        public static final int more_page_row4=0x7f0a003f;
+        public static final int more_page_row5=0x7f0a0042;
+        public static final int pay_pwd=0x7f0a0034;
+        public static final int pay_pwd_agin=0x7f0a0035;
+        public static final int regist_scroller=0x7f0a0021;
+        public static final int regist_scroller_relative=0x7f0a0017;
+        public static final int register_check_number=0x7f0a003b;
+        public static final int register_end=0x7f0a003a;
+        public static final int register_next=0x7f0a0020;
+        public static final int register_phone=0x7f0a001b;
+        public static final int register_txv_phone=0x7f0a001a;
+        public static final int remember_user=0x7f0a0025;
+        public static final int securityA_1=0x7f0a0037;
+        public static final int securityA_2=0x7f0a0039;
+        public static final int securityQ_1=0x7f0a0036;
+        public static final int securityQ_2=0x7f0a0038;
+        public static final int top_relative=0x7f0a003c;
+        public static final int tv_myCases_title=0x7f0a000f;
+        public static final int tv_title_back=0x7f0a0000;
+        public static final int txt_myCases_delte_txt=0x7f0a0009;
+        public static final int txt_myCases_selected_txt=0x7f0a0004;
+        public static final int txv_care_id=0x7f0a0018;
     }
     public static final class layout {
         public static final int about_me=0x7f030000;
@@ -195,64 +207,64 @@ public final class R {
         public static final int z_monney_user_rule=0x7f030020;
     }
     public static final class string {
-        public static final int action_settings=0x7f070003;
-        public static final int app_close=0x7f070001;
-        public static final int app_name=0x7f070000;
-        public static final int at=0x7f070006;
-        public static final int cancel=0x7f070002;
-        public static final int hello_world=0x7f070004;
-        public static final int main_home_page=0x7f070009;
-        public static final int main_message=0x7f070008;
-        public static final int main_more=0x7f07000b;
-        public static final int main_my_acount=0x7f070007;
-        public static final int main_safety_center=0x7f07000a;
-        public static final int text_Q_one=0x7f070025;
-        public static final int text_Q_sec=0x7f070026;
-        public static final int text_answer=0x7f070027;
-        public static final int text_card_class=0x7f07001d;
-        public static final int text_card_id=0x7f07001e;
-        public static final int text_check_number=0x7f070019;
-        public static final int text_consume_passwd=0x7f070024;
-        public static final int text_email_at=0x7f070021;
-        public static final int text_get_check_number=0x7f07001a;
-        public static final int text_login=0x7f070013;
-        public static final int text_login_display_passwd=0x7f070014;
-        public static final int text_login_forget_passwd=0x7f070012;
-        public static final int text_login_passwd=0x7f070022;
-        public static final int text_login_remenber_name=0x7f070015;
-        public static final int text_login_title=0x7f07000d;
-        public static final int text_login_user_name=0x7f07000e;
-        public static final int text_login_user_name_toast=0x7f07000f;
-        public static final int text_login_user_passwd=0x7f070010;
-        public static final int text_login_user_passwd_toast=0x7f070011;
-        public static final int text_message=0x7f070028;
-        public static final int text_my_account=0x7f070029;
-        public static final int text_once_agin=0x7f070023;
-        public static final int text_phone_number=0x7f070018;
-        public static final int text_register=0x7f070016;
-        public static final int text_sure=0x7f07000c;
-        public static final int text_user_email=0x7f070020;
-        public static final int text_user_name=0x7f07001b;
-        public static final int text_user_register=0x7f070017;
-        public static final int text_user_role=0x7f07001f;
-        public static final int text_user_true_name=0x7f07001c;
-        public static final int title_back=0x7f070005;
+        public static final int action_settings=0x7f080003;
+        public static final int app_close=0x7f080001;
+        public static final int app_name=0x7f080000;
+        public static final int at=0x7f080006;
+        public static final int cancel=0x7f080002;
+        public static final int hello_world=0x7f080004;
+        public static final int main_home_page=0x7f080009;
+        public static final int main_message=0x7f080008;
+        public static final int main_more=0x7f08000b;
+        public static final int main_my_acount=0x7f080007;
+        public static final int main_safety_center=0x7f08000a;
+        public static final int text_Q_one=0x7f080025;
+        public static final int text_Q_sec=0x7f080026;
+        public static final int text_answer=0x7f080027;
+        public static final int text_card_class=0x7f08001d;
+        public static final int text_card_id=0x7f08001e;
+        public static final int text_check_number=0x7f080019;
+        public static final int text_consume_passwd=0x7f080024;
+        public static final int text_email_at=0x7f080021;
+        public static final int text_get_check_number=0x7f08001a;
+        public static final int text_login=0x7f080013;
+        public static final int text_login_display_passwd=0x7f080014;
+        public static final int text_login_forget_passwd=0x7f080012;
+        public static final int text_login_passwd=0x7f080022;
+        public static final int text_login_remenber_name=0x7f080015;
+        public static final int text_login_title=0x7f08000d;
+        public static final int text_login_user_name=0x7f08000e;
+        public static final int text_login_user_name_toast=0x7f08000f;
+        public static final int text_login_user_passwd=0x7f080010;
+        public static final int text_login_user_passwd_toast=0x7f080011;
+        public static final int text_message=0x7f080028;
+        public static final int text_my_account=0x7f080029;
+        public static final int text_once_agin=0x7f080023;
+        public static final int text_phone_number=0x7f080018;
+        public static final int text_register=0x7f080016;
+        public static final int text_sure=0x7f08000c;
+        public static final int text_user_email=0x7f080020;
+        public static final int text_user_name=0x7f08001b;
+        public static final int text_user_register=0x7f080017;
+        public static final int text_user_role=0x7f08001f;
+        public static final int text_user_true_name=0x7f08001c;
+        public static final int title_back=0x7f080005;
     }
     public static final class style {
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
-        public static final int body=0x7f080003;
-        public static final int delete=0x7f080004;
+        public static final int AppTheme=0x7f090001;
+        public static final int body=0x7f090003;
+        public static final int delete=0x7f090004;
         /**  模糊 
          */
-        public static final int dialog=0x7f080009;
-        public static final int setting_attribute_list=0x7f080005;
-        public static final int setting_attribute_list_item_imageview_left=0x7f080006;
-        public static final int setting_attribute_list_item_imageview_right=0x7f080008;
-        public static final int setting_attribute_list_item_textview=0x7f080007;
-        public static final int top=0x7f080002;
+        public static final int dialog=0x7f090009;
+        public static final int setting_attribute_list=0x7f090005;
+        public static final int setting_attribute_list_item_imageview_left=0x7f090006;
+        public static final int setting_attribute_list_item_imageview_right=0x7f090008;
+        public static final int setting_attribute_list_item_textview=0x7f090007;
+        public static final int top=0x7f090002;
     }
 }
