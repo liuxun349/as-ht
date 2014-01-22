3a67108f71524a70e1689c7eb8cb27aad0c948f3
liuxun349 <lzliuxun@gmail.com>
2014-01-22 10:03:39 +0800

    merge llh

--- src/com/asht/controller/MainHomePageActivity.java
+++ src/com/asht/controller/MainHomePageActivity.java
@@ -3,15 +3,43 @@ package com.asht.controller;
 import com.asht.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
 
 public class MainHomePageActivity extends Activity {
 	private GridView mGridView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_page);
+		Controller.setNomePagTop(this, false, "互助抗癌");
+	}
+
+	public void onclick(View v) {
+		Intent intent = null;
+		intent = new Intent();
+		switch (v.getId()) {
+		case R.id.test_add_case:
+			intent.setClass(getApplicationContext(), AppStart.class);
+			break;
+		case R.id.test_add_cases:
+			intent.setClass(getApplicationContext(), AddCasesActivity.class);
+			break;
+		case R.id.test_case:
+			intent.setClass(getApplicationContext(),
+					MyCasesSingleActivity.class);
+			break;
+		case R.id.test_cases:
+			intent.setClass(getApplicationContext(), MyCasesActivity.class);
+			break;
+
+		default:
+			break;
+		}
+		startActivity(intent);
 	}
 }
